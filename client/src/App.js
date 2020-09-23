c53f779b0d720118dfa10445e1d9ed24273d4269
Jon Jackson <ocskier@gmail.com>
2020-09-22 20:58:31 -0400

    feat: added BE books API endpoint, FE fetch

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,12 @@ import React, { useEffect, useState } from 'react';
 import Header from './Components/Header';
 
 export const App = () => {
+  const [books, setBooks] = useState([]);
   const [time, setTime] = useState(null);
   const [error, setError] = useState(false);
 
   useEffect(() => {
-    async function fetchTime() {
+    async function fetchTimeAndBooks() {
       const response = await fetch('/current-time');
       if (response.ok) {
         const data = await response.json();
@@ -15,22 +16,34 @@ export const App = () => {
         date.setTime(data.time * 1000);
         date.toUTCString();
         setTime(date);
+        const booksResRaw = await fetch('/api/books')
+        const booksData = await booksResRaw.json();
+        setBooks(booksData);
       } else {
         setError(response.statusText);
       }
     }
-    fetchTime();
+    fetchTimeAndBooks();
   }, []);
 
   return (
-    <div className='App'>
+    <div className="App">
       <Header />
       {!error ? (
-        <p className='pt-2 text-center text-success'>
-          {!time ? 'Loading...' : `Server time: ${time}`}
-        </p>
+        <div>
+          <p className="pt-2 text-center text-success">
+            {!time ? "Loading..." : `Server time: ${time}`}
+          </p>
+          <ul>
+            {books.map((book,i) => (
+              <li key={i}>
+                {book.title} - {book.author}
+              </li>
+            ))}
+          </ul>
+        </div>
       ) : (
-        <p className='pt-2 text-center text-error'>{error}</p>
+        <p className="pt-2 text-center text-error">{error}</p>
       )}
     </div>
   );

94d9dfbe682f639be371f8b190fd3ac07d597d64
Jon Jackson <ocskier@gmail.com>
2020-11-02 22:08:32 -0500

    fix: Allow build push

---
+++ server/src/build/precache-manifest.e61e6968bb64160818fa6515b95309ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad35125ea3865e91734c68e035dd700",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e0bc4635c8d9f79a681",
+    "url": "/static/css/main.4f79c121.chunk.css"
+  },
+  {
+    "revision": "840c246e998d672757a6",
+    "url": "/static/js/2.90913833.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.90913833.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e0bc4635c8d9f79a681",
+    "url": "/static/js/main.e977a63e.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
